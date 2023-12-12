6e1f6832ac71f3514905a651c9c8067a7d7ea9e4
jahandar <saeid.jahandar@fau.de>
2023-12-12 12:38:39 +0000

    PEP8 and styling applied

--- qlog_plot.py
+++ qlog_plot.py
@@ -18,6 +18,7 @@ from matplotlib.ticker import EngFormatter
 import pandas as pd
 import seaborn as sns
 
+
 class QlogFormat(Enum):
     SQLOG = '.sqlog'
     QLOG = '.qlog'
@@ -38,7 +39,7 @@ def extract_data(qlog_dir, qlog_file):
     else:
         logging.error("The qlog file format is not supported!")
         return None, None, None, None
-    
+
     return create_dataframes(packets_list, metrics_list, offsets_list, datagram_list)
 
 
@@ -61,7 +62,7 @@ def create_dataframes(packets_list, metrics_list, offsets_list, datagram_list):
         if 'packet_number' in df_packets.columns:
             df_packets['duplicate'] = df_packets.duplicated(subset=['packet_number'])
             df_packets['packet_size_cumsum'] = df_packets.packet_size.cumsum()
-        
+
         if 'offset' in df_offsets.columns:
             df_offsets['duplicate'] = df_offsets.duplicated(subset=['offset'])
 
@@ -70,6 +71,7 @@ def create_dataframes(packets_list, metrics_list, offsets_list, datagram_list):
         return pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
     return df_packets, df_metrics, df_offsets, df_datagram
+
 
 def parse_quiche_log(qlog_dir, qlog_file):
     """
@@ -88,7 +90,7 @@ def parse_quiche_log(qlog_dir, qlog_file):
     try:
         with open(os.path.join(qlog_dir, qlog_file), 'r') as json_file:
             content = json_file.read()
-            
+
             # split the input data into individual JSON texts
             # \u001E is the ASCII Record Separator (RS) character
             json_objects = content.split('\u001E')
@@ -98,19 +100,20 @@ def parse_quiche_log(qlog_dir, qlog_file):
             if first_json_object:
                 first_json_data = json.loads(first_json_object)
                 role = first_json_data.get('title', None)
-            
+
             packet_direction = get_packet_direction(role)
 
             # Process remaining JSON objects
             for json_object in json_objects:
                 json_object = json_object.strip()   # remove the line feed at the end of the json_text
                 if json_object:  # check the string is not empty
-                    process_quiche_json_object(json_object, packet_direction, packets_list, metrics_list, offsets_list, datagram_list)
+                    process_quiche_json_object(json_object, packet_direction, packets_list,
+                                               metrics_list, offsets_list, datagram_list)
 
     except json.JSONDecodeError as e:
         logging.error(f"Error decoding JSON for file {qlog_file}: {e}")
         return [], [], [], []
-    
+
     return packets_list, metrics_list, offsets_list, datagram_list
 
 
@@ -128,8 +131,8 @@ def process_quiche_json_object(json_object, packet_direction, packets_list, metr
         json_seq = json.loads(json_object)
 
         if json_seq.get('name') == f'transport:{packet_direction}':
-            packet_dict = {'time': json_seq['time'] * 1000, 
-                           'packet_number': json_seq['data']['header']['packet_number'], 
+            packet_dict = {'time': json_seq['time'] * 1000,
+                           'packet_number': json_seq['data']['header']['packet_number'],
                            'packet_size': json_seq['data']['raw']['length']}
             packets_list.append(packet_dict)
 
@@ -137,18 +140,16 @@ def process_quiche_json_object(json_object, packet_direction, packets_list, metr
             for frame in frames:
                 if frame['frame_type'] == 'stream':
                     offset_dict = {'time': json_seq['time'] * 1000,
-                                    'offset': frame['offset'],
-                                    'length': frame['length'],
-                                    'goodput': None,
-                                    'goodput_no_gaps': None}
+                                   'offset': frame['offset'],
+                                   'length': frame['length'],
+                                   'goodput': None,
+                                   'goodput_no_gaps': None}
                     offsets_list.append(offset_dict)
-
 
             datagram_dict = {'time': json_seq['time'] * 1000,
                              'length': json_seq['data']['raw']['length'],
                              'throughput': None}
             datagram_list.append(datagram_dict)
-
 
         if json_seq.get('name') == 'recovery:metrics_updated':
             for key in json_seq['data']:
@@ -192,11 +193,11 @@ def parse_picoquic_log(qlog_dir, qlog_file):
 
             for event in events:
                 process_picoquic_event(event, packet_direction, packets_list, metrics_list, offsets_list, datagram_list)
-            
+
     except json.JSONDecodeError as e:
         logging.error(f"Error decoding JSON for file {qlog_file}: {e}")
         return [], [], [], []
-    
+
     return packets_list, metrics_list, offsets_list, datagram_list
 
 
@@ -229,31 +230,31 @@ def process_picoquic_event(event, packet_direction, packets_list, metrics_list, 
     """
     if event[1] == 'transport' and event[2] == packet_direction:
         packet_dict = {'time': event[0],
-                        'packet_number': event[3]['header']['packet_number'],
-                        'packet_size': event[3]['header']['packet_size']}
+                       'packet_number': event[3]['header']['packet_number'],
+                       'packet_size': event[3]['header']['packet_size']}
         packets_list.append(packet_dict)
 
         frames = event[3]['frames']
         for frame in frames:
             if frame['frame_type'] == 'stream':
                 offset_dict = {'time': event[0],
-                                'offset': frame['offset'],
-                                'length': frame['length'],
-                                'goodput': None,
-                                'goodput_no_gaps': None}
+                               'offset': frame['offset'],
+                               'length': frame['length'],
+                               'goodput': None,
+                               'goodput_no_gaps': None}
                 offsets_list.append(offset_dict)
 
         datagram_dict = {'time': event[0],
                          'length': event[3]['header']['packet_size'],
                          'throughput': None}
         datagram_list.append(datagram_dict)
-    
+
     if event[1] == 'recovery' and event[2] == 'metrics_updated':
         for key in event[3]:
             metric_dict = {'time': event[0],
-                            'key': key,
-                            'value': event[3][key]}
-            metrics_list.append(metric_dict)    
+                           'key': key,
+                           'value': event[3][key]}
+            metrics_list.append(metric_dict)
 
 
 def get_time_window_size(last_time):
@@ -275,7 +276,7 @@ def calculate_throughput_goodput(df, metric):
         interval_end = interval_start + time_window_size
         while interval_end <= last:
             interval = df[(df['time'] >= interval_start)
-                        & (df['time'] < interval_end)]
+                          & (df['time'] < interval_end)]
             if not interval.empty:
                 interval_sum = interval['length'].sum()
                 metric_val = interval_sum / time_window_size
@@ -287,9 +288,9 @@ def calculate_throughput_goodput(df, metric):
             else:
                 interval_start = interval_start + time_window_size
                 interval_end = interval_start + time_window_size
-        
+
         return df
-    
+
     except Exception as e:
         logging.error(f"Error in calculate_throughput_goodput: {e}")
         return pd.DataFrame()
@@ -435,14 +436,7 @@ def process_file(qlog_dir, qlog_file):
             df_offsets,
             df_datagram,
             qlog_file)
-        save_data_and_figures(
-            df_packets,
-            df_metrics,
-            df_offsets,
-            df_datagram,
-            fig,
-            qlog_file,
-            qlog_dir)
+        save_data_and_figures(df_packets, df_metrics, df_offsets, df_datagram, fig, qlog_file, qlog_dir)
 
         elapsed_time = time.time() - start_time
         return qlog_file, elapsed_time
@@ -465,12 +459,11 @@ def process_data(df_datagram, df_offsets):
         # df_offsets[df_offsets['duplicate'] == False] = calculate_throughput_goodput(df_offsets, 'goodput')
 
         return df_datagram_processed, df_offsets_processed
-    
+
     except Exception as e:
         logging.error(f"Error in processing data frames: {e}")
         # Return original data frames in case of error
         return df_datagram, df_offsets
-    
 
 
 def main():
@@ -492,14 +485,14 @@ def main():
     args = parser.parse_args()
 
     logging.info("Expected file format: filename.[QUIC logging format]")
-    
+
     start_time_total = time.time()
 
     if args.file:
         process_single_file(args.qlog_dir, args.file)
     else:
         process_all_files(args.qlog_dir)
-        
+
     logging.info(f"Total run time: {time.time() - start_time_total}")
 
 
@@ -511,7 +504,7 @@ def process_single_file(qlog_dir, qlog_file):
     :param qlog_file: Name of the qlog file to process.
     """
     file_path = os.path.join(qlog_dir, qlog_file)
-    
+
     if not os.path.exists(file_path):
         logging.error(f"File not found: {file_path}")
         return
@@ -534,7 +527,7 @@ def process_all_files(qlog_dir):
     :param qlog_dir: Directory containing the qlog files.
     """
     list_qlog_files = [f for f in os.listdir(qlog_dir) if is_valid_file(f)]
-    
+
     if not list_qlog_files:
         logging.info("No valid qlog files found in the directory.")
         return
@@ -563,7 +556,7 @@ def is_valid_file(filename):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    #TODO: Add a switch for enabling debug mode
+    # TODO: Add a switch for enabling debug mode
     # logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     try:
         main()
